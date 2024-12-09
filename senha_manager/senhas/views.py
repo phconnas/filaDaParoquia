4cb72c2273b5aaf6c8b9d3fbaad1373a01f3949d
Paulo Nascimento <phconnas@gmail.com>
2024-12-09 14:53:04 -0300

    alteracoes de admin

--- senha_manager/senhas/views.py
+++ senha_manager/senhas/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Senha
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def listar_senhas(request):
     senhas = Senha.objects.order_by('-criada_em')
     return render(request, 'senhas/listar_senhas.html', {'senhas': senhas})
 
+@login_required
 def chamar_senha(request, senha_id):
     senha = Senha.objects.get(id=senha_id)
     senha.chamada = True
@@ -12,10 +16,12 @@ def chamar_senha(request, senha_id):
     messages.success(request, f"Senha {senha.numero} chamada com sucesso!")
     return redirect('listar_senhas')
 
+@login_required
 def senha_chamada(request):
     senha_atual = Senha.objects.filter(chamada=True).order_by('-criada_em').first()
     return render(request, 'senhas/senha_chamada.html', {'senha_atual': senha_atual})
 
+@login_required
 def gerar_senha(request):
     ultima_senha = Senha.objects.order_by('-numero').first()
     novo_numero = (ultima_senha.numero + 1) if ultima_senha else 1
@@ -23,6 +29,7 @@ def gerar_senha(request):
     messages.success(request, f"Nova senha gerada: {novo_numero}")
     return redirect('listar_senhas')
 
+@login_required
 def resetar_senhas(request):
     Senha.objects.all().delete()
     messages.warning(request, "Todas as senhas foram resetadas.")
